9440ab12f567014670843144d836527fe7b0a602
nickolanack <nickblackwell82@gmail.com>
2023-02-21 09:07:55 -0800

    add comments to update .php

--- web/update.php
+++ web/update.php
@@ -1,4 +1,9 @@
 <?php 
 
+/**
+ * @TODO: 
+ * - Configure github webhook to point here
+ * - On commit auto pull repos
+ */
 
 file_put_contents('../webhooks.txt', json_encode($_POST, JSON_PRETTY_PRINT)."\n", FILE_APPEND);
